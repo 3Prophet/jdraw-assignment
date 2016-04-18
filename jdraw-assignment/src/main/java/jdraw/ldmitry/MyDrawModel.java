0a09aa58affaf613c610657855272d8661404326
Dmitry Logvinovich <dlogvinovich@yahoo.com>
2016-04-18 23:53:35 +0200

    Implemented MyDrawModel

--- jdraw-assignment/src/main/java/jdraw/ldmitry/MyDrawModel.java
+++ jdraw-assignment/src/main/java/jdraw/ldmitry/MyDrawModel.java
@@ -5,12 +5,16 @@
 
 package jdraw.ldmitry;
 
+import java.util.Collections;
 import java.util.LinkedList;
 
 import jdraw.framework.DrawCommandHandler;
 import jdraw.framework.DrawModel;
+import jdraw.framework.DrawModelEvent;
 import jdraw.framework.DrawModelListener;
 import jdraw.framework.Figure;
+import jdraw.framework.FigureEvent;
+import jdraw.framework.FigureListener;
 import jdraw.std.EmptyDrawCommandHandler;
 
 /**
@@ -20,42 +24,65 @@ import jdraw.std.EmptyDrawCommandHandler;
  * @author TODO add your name here
  *
  */
-public class MyDrawModel implements DrawModel {
+public class MyDrawModel implements DrawModel, FigureListener {
 	
 	private LinkedList<Figure> figures = new LinkedList<Figure>();
 	
 	private LinkedList<DrawModelListener> listeners = new LinkedList<DrawModelListener>();
-
+	
+	/**
+	 * Adds figure to the list of figures. Registers itself as a FigureListener of a figure
+	 * @param f figure to be added
+	 */
 	@Override
 	public void addFigure(Figure f) {
-		// TODO to be implemented
-		System.out.println("StdDrawModel.addFigure has to be implemented");
+		if (f != null) {
+			f.addFigureListener(this);
+			figures.add(f);
+			notifyListeners(f, DrawModelEvent.Type.FIGURE_ADDED);
+		}
 	}
-
+	
+	/**
+	 * @return Read-only copy of the list of figures
+	 */
 	@Override
 	public Iterable<Figure> getFigures() {
-		// TODO to be implemented  
-		System.out.println("StdDrawModel.getFigures has to be implemented");
-		return new LinkedList<Figure>(); // Only guarantees, that the application starts -- has to be replaced !!!
+		return Collections.unmodifiableList(figures); 
 	}
-
+	
+	/**
+	 * Removes a particular figure from the list of observed figures
+	 * @param f figure to be removed
+	 */
 	@Override
 	public void removeFigure(Figure f) {
-		// TODO to be implemented  
-		System.out.println("StdDrawModel.removeFigure has to be implemented");
+		if (figures.contains(f)) {
+			figures.remove(f);
+			notifyListeners(f, DrawModelEvent.Type.FIGURE_REMOVED);
+		}
 	}
-
+	
+	/**
+	 * Adds new listener to the list of listeners
+	 * @param listener 
+	 */
 	@Override
 	public void addModelChangeListener(DrawModelListener listener) {
-		listeners.add(listener);
-		// TODO to be implemented  
-		System.out.println("StdDrawModel.addModelChangeListener has to be implemented");
+		if (listener != null) {
+			listeners.add(listener);
+		}
 	}
-
+	
+	/**
+	 * Removes a particular listener from the list of listeners
+	 * @param listener to be removed
+	 */
 	@Override
 	public void removeModelChangeListener(DrawModelListener listener) {
-		// TODO to be implemented  
-		System.out.println("StdDrawModel.removeModelChangeListener has to be implemented");
+		if (listeners.contains(listener)) {
+			listeners.remove(listener);
+		}
 	}
 
 	/** The draw command handler. Initialized here with a dummy implementation. */
@@ -75,11 +102,43 @@ public class MyDrawModel implements DrawModel {
 		// TODO to be implemented  
 		System.out.println("StdDrawModel.setFigureIndex has to be implemented");
 	}
-
+	
+	/**
+	 * Removing all the observable figures from the figures list
+	 */
 	@Override
-	public void removeAllFigures() {
-		// TODO to be implemented  
-		System.out.println("StdDrawModel.removeAllFigures has to be implemented");
+	public void removeAllFigures() {	
+		int size = figures.size();
+		while (size != 0) {
+			Figure f = figures.pop();
+			--size;
+			notifyListeners(f, DrawModelEvent.Type.FIGURE_REMOVED);
+		}
 	}
-
+	
+	/**
+	 * Handler for notification events from observable figures
+	 * @param e
+	 */
+	@Override
+	public void figureChanged(FigureEvent e) {
+		if (e != null) {
+			notifyListeners(e.getFigure(), 
+					DrawModelEvent.Type.FIGURE_CHANGED);
+		}
+	}
+	
+	/**
+	 * Notifies all subscribed DrawModelListeners on a model change
+	 * @param e 
+	 */
+	public void notifyListeners(Figure f, 
+			DrawModelEvent.Type type) {
+		if (listeners.size() != 0) {
+			DrawModelEvent dme = new DrawModelEvent(this, f, type);
+			for (DrawModelListener l: listeners) {
+				l.modelChanged(dme);				
+			}
+		}
+	}
 }
