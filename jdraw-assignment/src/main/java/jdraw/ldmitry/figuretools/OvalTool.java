f6ddce18183d17dd0ba5164ec02aeb839cda8ac2
Dmitry Logvinovich <dlogvinovich@yahoo.com>
2016-04-24 20:21:30 +0200

    Refactored OvalTool

--- jdraw-assignment/src/main/java/jdraw/ldmitry/figuretools/OvalTool.java
+++ jdraw-assignment/src/main/java/jdraw/ldmitry/figuretools/OvalTool.java
@@ -10,92 +10,32 @@ import javax.swing.ImageIcon;
 import jdraw.framework.DrawContext;
 import jdraw.framework.DrawTool;
 import jdraw.framework.DrawView;
+import jdraw.framework.Figure;
 import jdraw.ldmitry.figures.Oval;
 
-public class OvalTool implements DrawTool {
-	/** 
-	 * the image resource path. 
-	 */
-	private static final String IMAGES = "/images/";
-	/**
-	 * The context we use for drawing.
-	 */
-	private DrawContext context;
+public class OvalTool extends AbstractFigureTool {
 
-	/**
-	 * The context's view. This variable can be used as a shortcut, i.e.
-	 * instead of calling context.getView().
-	 */
-	private DrawView view;
-
-	/**
-	 * Temporary variable. During oval creation (during a
-	 * mouse down - mouse drag - mouse up cycle) this variable refers
-	 * to the new oval that is inserted.
-	 */
-	private Oval newOval = null;
-
-	/**
-	 * Temporary variable.
-	 * During oval creation this variable refers to the point the
-	 * mouse was first pressed.
-	 */
-	private Point anchor = null;
 	/**
 	 * Create a new oval tool for the given context.
 	 * @param context a context to use this tool in.
 	 */
 	public OvalTool(DrawContext context) {
-		this.context = context;
-		this.view = context.getView();
+		super(context);
 	}
 
 	@Override
-	public void activate() {
-		this.context.showStatusText("Oval Mode");
+	public Figure getNewFigure() {
+		return new Oval((float) getAnchor().getX(), (float) getAnchor().getY(), 0, 0);
 	}
-
-	@Override
-	public void deactivate() {
-		this.context.showStatusText("");	
-	}
-
-	@Override
-	public void mouseDown(int x, int y, MouseEvent e) {
-		if (newOval != null) {
-			throw new IllegalStateException();
-		}
-		anchor = new Point(x, y);
-		newOval = new Oval(x, y, 0, 0);
-		view.getModel().addFigure(newOval);	
-	}
-
-	@Override
-	public void mouseDrag(int x, int y, MouseEvent e) {
-		newOval.setBounds(anchor, new Point(x, y));
-		java.awt.Rectangle r = newOval.getBounds();
-		this.context.showStatusText("w: " + r.width + ", h: " + r.height);	
-	}
-
-	@Override
-	public void mouseUp(int x, int y, MouseEvent e) {
-		newOval = null;
-		anchor = null;
-		this.context.showStatusText("Oval Mode");	
-	}
-
-	@Override
-	public Cursor getCursor() {
-		return Cursor.getPredefinedCursor(Cursor.CROSSHAIR_CURSOR);
-	}
-
-	@Override
-	public Icon getIcon() {
-		return new ImageIcon(getClass().getResource(IMAGES + "oval.png"));
-	}
-
-	@Override
-	public String getName() {
-		return "Oval";
-	}
+	
+	/**
+	 * Factory method for the OvalTool.
+	 * @param context context that registers an OvalTool
+	 * @return
+	 */
+	public static DrawTool getDrawTool(DrawContext context) {
+		OvalTool otool = new OvalTool(context);
+		otool.setName("Oval");
+		return otool;
+	}	
 }
